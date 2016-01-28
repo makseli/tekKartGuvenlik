7704cf05b14b6869e32cf1ebbf9273b9275e4c1a
makseli <verilojistik@gmail.com>
2016-01-28 16:15:07 +0200

    son çalışan hali

--- ekranKlavyesiKur.py
+++ ekranKlavyesiKur.py
@@ -8,13 +8,13 @@ class bcolors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-print(bcolors.FAIL+" \n LUTFEN yeniSite.py dosyasini SUDO izni ile calistirdiginiza emin olunuz  \n"+bcolors.ENDC)
-print(bcolors.OKBLUE+" \n LUTFEN yeniSite.py dosyasini SUDO izni ile calistirdiginiza emin olunuz  \n"+bcolors.ENDC)
+print(bcolors.FAIL+" \n LUTFEN bu dosyayı SUDO izni ile calistirdiginiza emin olunuz  \n"+bcolors.ENDC)
+print(bcolors.OKBLUE+" \n LUTFEN bu dosyayı SUDO izni ile calistirdiginiza emin olunuz  \n"+bcolors.ENDC)
 
 import os;
 os.system("sudo apt-get install -y matchbox-keyboard")
 
-print(bcolors.OKGREEN+" matchbox-keyboard kuruldu !\n"+bcolors.ENDC)
+print(bcolors.OKGREEN+" \n \n matchbox-keyboard kuruldu !\n"+bcolors.ENDC)
 
 metin = """
 #!/bin/bash
@@ -23,9 +23,9 @@ matchbox-keyboard
 
 
 os.getcwd()
-VHostDosya = open("/home/pi/Desktop/klavye_calistir.conf", "w")
+VHostDosya = open("/home/pi/Desktop/klavye_calistir.sh", "w")
 VHostDosya.write(metin)
 VHostDosya.close();
 
-print(bcolors.OKBLUE+"yeni kısayol dosyası masaüstünde olusturuldu !\n"+bcolors.ENDC)
+print(bcolors.OKBLUE+" \n yeni kısayol dosyası masaüstünde olusturuldu ! \n \n"+bcolors.ENDC)
 exit();
